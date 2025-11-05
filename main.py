a26b41dafeae96f25f7b516eec758a09f2197d5c
bollamrakeshkumar2000 <bollamrakeshkumar2000@gmail.com>
2025-11-05 11:45:55 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import demographic_data_analyzer
+
+# Test the function
+demographic_data_analyzer.calculate_demographic_data()
+
+# Run unit tests (if available)
+# from unittest import main
+# main(module='test_module', exit=False)
